542b28de14bac20ac0ee4bfc31fa37b8a885af03
arafato <oliver.arafat@outlook.com>
2016-04-29 14:13:07 +0200

    recursive merge

--- test/httpTrigger/httpTriggerNode.js
+++ test/httpTrigger/httpTriggerNode.js
@@ -1,5 +1,5 @@
 module.exports = function(context, req) {
-    context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
+    context.log('Node.js HTTP trigger function processed a request. RequestUri=' + req.originalUrl);
 
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
